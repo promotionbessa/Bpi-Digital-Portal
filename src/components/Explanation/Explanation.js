be055a490aa846cc8068bb6188b1c14be59cfbd5
PC-Dahmoun Djamel <37736511+bachi-mohamed-rafik@users.noreply.github.com>
2024-11-04 13:28:33 +0100

    fixing explanation padding

--- src/components/Explanation/Explanation.js
+++ src/components/Explanation/Explanation.js
@@ -14,17 +14,15 @@ const Explanation = () => {
       <p >
         - Le <span className="green-ribbon">ruban vert</span> avec le mot <strong>"Disponible"</strong> signifie que le service fonctionne correctement et est accessible sans problème.
         </p>
-      <p className='guide-content'>
+      <p>
         - Le <span className="red-ribbon">ruban rouge</span> avec le mot <strong>"Indisponible"</strong> indique que le service est temporairement hors ligne ou indisponible en raison d'une panne ou d'un problème technique.
       </p>
-      <div className='guide-content'>
         <p>
           - Accessible Globalement: Disponible sur le cloud et accessible de n'importe où
         </p>
         <p>
           - Accessible Localement: Disponible uniquement sur le serveur local
         </p>
-      </div>
       </text>
       </div>
 
