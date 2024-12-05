ace341c74a364ea9f1ffe29701552683b932f6fa
promotionbessa <32738562+promotionbessa@users.noreply.github.com>
2024-12-05 15:01:41 +0100

    update

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -16,136 +16,136 @@ import { useState } from 'react';
 import Hover from '../Hover/Hover';
 
 const services = [
-  { 
-    name: "Odoo", 
-    description : "Odoo production base",
+  {
+    name: "Odoo",
+    description: "Odoo production base",
     logo: odoo,
-    state : true,
-    accessibility :true
+    state: true,
+    accessibility: true
   },
-  { 
-    name: "Biotime", 
+  {
+    name: "Biotime",
     description: "Time management software",
     logo: Biotime,
-    state : true,
-    accessibility :true
+    state: true,
+    accessibility: true
   },
-  { 
-    name: "Glpi", 
+  {
+    name: "Glpi",
     description: "IT asset management system",
     logo: Glpi,
-    state : true,
-    accessibility :true,
+    state: true,
+    accessibility: true,
   },
-  { 
-    name: "Bessa promotion", 
+  {
+    name: "Bessa promotion",
     logo: BessaPro,
-    description : "Bessa Promotion Immobiliére",
-    state : true,
-    accessibility :true,  
+    description: "Bessa Promotion Immobiliére",
+    state: true,
+    accessibility: true,
   },
-  { 
-    name: "Odoo Test", 
-    logo: odooTest ,
+  {
+    name: "Odoo Test",
+    logo: odooTest,
     description: "Testing environment for Odoo",
-    state : true,
-    accessibility :true,      
+    state: true,
+    accessibility: true,
   },
-  { 
-    name: "ReportingDB", 
-    logo: postgre ,
+  {
+    name: "ReportingDB",
+    logo: postgre,
     description: "Reporting DataBase",
-    state : true,
-    accessibility :false,      
+    state: true,
+    accessibility: false,
   },
-  { 
-    name: "ODOO Simonto", 
-    logo: Simonto ,
+  {
+    name: "ODOO Simonto",
+    logo: Simonto,
     description: "Simonto management in Odoo",
-    state : true,
-    accessibility :true,      
+    state: true,
+    accessibility: true,
   },
-  { 
-    name: "Assistance IT/ODOO", 
-    logo: odooSupport ,
+  {
+    name: "Assistance IT/ODOO",
+    logo: odooSupport,
     description: "IT and Odoo support service",
-    state : true,
-    accessibility :true,        
+    state: true,
+    accessibility: true,
   },
-  { 
-    name: "Google", 
-    logo: google ,
+  {
+    name: "Google",
+    logo: google,
     description: "Lien vers google",
-    state : true,
-    accessibility :true,        
+    state: true,
+    accessibility: true,
   },
-  { 
-    name: "Hubspot", 
-    logo: hubspot ,
+  {
+    name: "Hubspot",
+    logo: hubspot,
     description: "Lien vers hubspot",
-    state : true,
-    accessibility :true,        
+    state: true,
+    accessibility: true,
   },
-  { 
-    name: "Issabel", 
-    logo: issabel1 ,
+  {
+    name: "Issabel",
+    logo: issabel1,
     description: "Lien vers issabel",
-    state : true,
-    accessibility :true,        
-},
-{ 
-  name: "odooSupport", 
-  logo: odooSupport ,
-  description: "Lien vers issabel",
-  state : true,
-  accessibility :true,        
-},
-{ 
-  name: "helpDesk", 
-  logo: helpDesk ,
-  description: "Lien vers helpDesk",
-  state : true,
-  accessibility :true,        
-}
+    state: true,
+    accessibility: true,
+  },
+  {
+    name: "odooSupport",
+    logo: odooSupport,
+    description: "Lien vers issabel",
+    state: true,
+    accessibility: true,
+  },
+  {
+    name: "helpDesk",
+    logo: helpDesk,
+    description: "Lien vers helpDesk",
+    state: true,
+    accessibility: true,
+  }
 
 ]
 
-const Card = ({ name, description,  link, state, accessibility }) => {
+const Card = ({ name, description, link, state, accessibility }) => {
   const foundService = services.find((item) => (item.name === name));
   const [hidden, setHidden] = useState(true);
 
   return (
     <div
-    onMouseEnter={() => setHidden(false)}
-    onMouseLeave={() => setHidden(true)}
+      onMouseEnter={() => setHidden(false)}
+      onMouseLeave={() => setHidden(true)}
     >
-    <a href={link} target="_blank" rel="noopener noreferrer" className="card-link">
+      <a href={link} target="_blank" rel="noopener noreferrer" className="card-link">
 
-    <div className="card" >
+        <div className="card" >
 
 
-<div className='card-test'>
-  <img src={foundService.logo} alt={`${foundService.name} logo`} className="card-image" />
-  {
-  state === true ?
-          <div className="card-badge-bg-success"></div>
-          :
-          <div className="card-badge-bg-danger"></div>
-      }
-<div class="hover-down-arrow"></div>
-</div>
+          <div className='card-test'>
+            <img src={foundService.logo} alt={`${foundService.name} logo`} className="card-image" />
+            {
+              state === true ?
+                <div className="card-badge-bg-success"></div>
+                :
+                <div className="card-badge-bg-danger"></div>
+            }
+            <div class="hover-down-arrow"></div>
+          </div>
 
-    </div>
-    {hidden ? null : 
-    <Hover 
-    name={name}
-    description={description}
-    state={state}
-    accessibility={accessibility}
-    />
-    }
+        </div>
+        {hidden ? null :
+          <Hover
+            name={name}
+            description={description}
+            state={state}
+            accessibility={accessibility}
+          />
+        }
 
-    </a>
+      </a>
 
     </div>
 
